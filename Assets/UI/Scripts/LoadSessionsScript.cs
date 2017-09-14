d3904da4416855b1e378df5736d97bedbda52e2b
Christian W <pin0ychris1810@gmail.com>
2017-09-14 20:29:43 +0200

    menu updates

---
+++ Assets/UI/Scripts/LoadSessionsScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadSessionsScript : MonoBehaviour {
+    [SerializeField] GameObject buttonPrefab;
+    [SerializeField] Transform sessionsPanel;
+
+    // Use this for initialization
+    void Start () {
+		for(int i = 0; i < 4;i++)
+        {
+            GameObject sessionButton = (GameObject)Instantiate(buttonPrefab);
+            sessionButton.GetComponentInChildren<Text>().text = "Yay Game";
+            sessionButton.transform.parent = sessionsPanel;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
