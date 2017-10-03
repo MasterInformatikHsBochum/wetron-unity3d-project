dd68a35c17447a5c8bb46a222e85a0d7c8e918c8
Mario Groneick <Mario.groneick@googlemail.com>
2017-10-03 10:56:45 +0200

    GScene = Preview Szene für Grafischen Kram

---
+++ Assets/Models/Arena/ColorTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTest : MonoBehaviour {
+	public Material changeMat;
+	// Use this for initialization
+	void Start () {
+		foreach (Renderer comp in GetComponentsInChildren<Renderer>()) {
+			//Debug.Log (comp.material.name);
+			if (comp.sharedMaterial == changeMat ) {
+				Debug.Log ("true :" + comp);
+				comp.material.color = Color.blue;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Models/lightChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightChange : MonoBehaviour {
+	private Light light;
+	private bool trigger = true;
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.light.intensity >= 1)
+			trigger = true;
+		
+		if (this.light.intensity == 0) 
+			trigger = false;
+		
+
+
+
+		if (trigger == true) {
+			this.light.intensity -= 0.005f;
+		} if (trigger == false) {
+			this.light.intensity += 0.005f;
+		}
+	}
+}
