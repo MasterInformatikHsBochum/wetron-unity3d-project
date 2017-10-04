1b98420bc4e44fc27c54dcc8033aece498514837
Mario Groneick <Mario.groneick@googlemail.com>
2017-10-04 16:46:20 +0200

    no message

--- Assets/Models/Arena/ColorTest.cs
+++ Assets/Models/Arena/ColorTest.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class ColorTest : MonoBehaviour {
 	public Material changeMat;
+	private Color color;
 	// Use this for initialization
 	void Start () {
 		foreach (Renderer comp in GetComponentsInChildren<Renderer>()) {
 			//Debug.Log (comp.material.name);
-			if (comp.sharedMaterial == changeMat ) {
+			if (comp.sharedMaterial == changeMat) {
 				Debug.Log ("true :" + comp);
 				comp.material.color = Color.blue;
 			}
 		}
+		foreach (TrailRenderer comp in GetComponents<TrailRenderer>()) {
+			Debug.Log ("comp : " + comp.ToString ());
+
+			for(int i = 0; i < comp.colorGradient.colorKeys.Length; i++ ) {
+				Debug.Log ("First color :" + comp.colorGradient.colorKeys [i].color);
+				comp.colorGradient.colorKeys[i].color = Color.blue;
+				Debug.Log ("Second color :" + comp.colorGradient.colorKeys [i].color);
+			}
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
