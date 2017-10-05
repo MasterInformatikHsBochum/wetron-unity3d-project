d57e3d73f09900a4db2a5e26d214bf0b3bafa1f6
Mario Groneick <Mario.groneick@googlemail.com>
2017-10-05 07:40:16 +0200

    Faben für die Spieler - muss nurnoch im Gamemanager eingebunden werden

--- Assets/Models/Arena/ColorTest.cs
+++ Assets/Models/Arena/ColorTest.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class ColorTest : MonoBehaviour {
 	public Material changeMat;
-	public Color pColor;
+	public Color[] pColor;
 	void Start () {
-		pColor = Color.blue;
+		pColor [0] = Color.blue;
+		pColor [1] = Color.red;
+		pColor [2] = Color.cyan;
+		pColor [3] = Color.green;
+		pColor [4] = Color.yellow;
+		pColor [5] = Color.magenta;
+	}
 
-		foreach (TrailRenderer comp in GetComponents<TrailRenderer>()) {
-		//	comp.colorGradient.SetKeys(grdKey, grdAlpha);
-			comp.startColor = pColor;
-			comp.endColor = pColor;
+	public void calcColorPlayer(GameObject model, int playerID){
+		int i = playerID % 6;
+		foreach (TrailRenderer comp in model.GetComponents<TrailRenderer>()) {
+			//	comp.colorGradient.SetKeys(grdKey, grdAlpha);
+			comp.startColor = pColor[i];
+			comp.endColor = pColor[i];
 		}
 
-		foreach (Renderer comp in GetComponentsInChildren<Renderer>()) {
+		foreach (Renderer comp in model.GetComponentsInChildren<Renderer>()) {
 			//Debug.Log (comp.material.name);
 			if (comp.sharedMaterial == changeMat) {
-				comp.material.color = pColor;
+				comp.material.color = pColor[i];
 				//comp.material.color = new Color(1.0f, 0.982f, 0.982f, 1.0f);
 			}
 		}
 
 	}
-
 	// Update is called once per frame
 	void Update () {
 		
